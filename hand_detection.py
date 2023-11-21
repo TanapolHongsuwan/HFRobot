eeebf9e990aba0697619cd0cc9529ba049323803
pfboy77 <s19C1105LW@s.chibakoudai.jp>
2023-11-21 19:33:03 +0900

    表示頻度の調整

--- hand_detection.py
+++ hand_detection.py
@@ -79,7 +79,7 @@ while cap.isOpened():
 
     # ウィンドウに映像を表示
     cv2.imshow('MediaPipe Hands', image)
-    if cv2.waitKey(5) & 0xFF == 27:
+    if cv2.waitKey(250) & 0xFF == 27:
         break
 
 # リソースを解放
