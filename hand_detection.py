2cc9376ab327ce09dd4525cd88673649e6561436
pfboy77 <s19C1105LW@s.chibakoudai.jp>
2023-11-24 18:23:02 +0900

    じゃんけんの判定をできるようにした。手と顔を同時に表示できるようにした。

--- hand_detection.py
+++ hand_detection.py
@@ -2,17 +2,9 @@ import cv2
 import pygame
 import numpy as np
 import mediapipe as mp
-#from face import draw_robot_face
-
-# MediaPipeの手の検出モジュールを初期化
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
-mp_drawing = mp.solutions.drawing_utils
-
 
 def LandmarksToNumpy(landmarks):
     return np.array([[landmark.x, landmark.y, landmark.z] for landmark in landmarks], dtype=np.float32)
-
 
 def CalculateAngle(v1, v2):
     dot_product = np.dot(v1, v2)
@@ -32,131 +24,57 @@ def FingerAngle(landmarks_array):
     return hand_angles
 
 def HandshapeJudge(hand_angles):
-    hand_shape = "NoSign"
+    hand_shape_num = 0
+    hand_shape = "happy"
 
     if hand_angles[1] < 90 and hand_angles[2] < 90 and hand_angles[3] < 90 and hand_angles[4] < 90:
+        hand_shape_num = 0
         hand_shape = "happy"
         #hand_shape = "Rock"
     elif hand_angles[1] >= 90 and hand_angles[2] >= 90 and hand_angles[3] < 90 and hand_angles[4] < 90:
+        hand_shape_num = 1
         hand_shape = "sad"
         #hand_shape = "Scissors"
     elif hand_angles[1] >= 90 and hand_angles[2] >= 90 and hand_angles[3] >= 90 and hand_angles[4] >= 90:
+        hand_shape_num = 2
         hand_shape = "angry"
         #hand_shape = "Paper"
 
-    return hand_shape
+    return hand_shape, hand_shape_num
 
-# ウェブカメラからの映像を取得
-cap = cv2.VideoCapture(0)
-pygame.init()
+def GetHandShape():
+    # ウェブカメラからの映像を取得
+    cap = cv2.VideoCapture(0)
 
+    # MediaPipeの手の検出モジュールを初期化
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands()
 
-# Set the display to full screen
-infoObject = pygame.display.Info()
-width, height = infoObject.current_w, infoObject.current_h
-screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            continue
 
-# Define colors
-WHITE = (255, 255, 255)
-LIGHT_BLUE = (173, 216, 230)  # Color for the eyes
-BLACK = (0, 0, 0)
-GRAY = (192, 192, 192)  # Color for the face
+        # 画像をBGRからRGBに変換し、MediaPipeに渡す
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
 
-# Set up robot face properties
-face_color = WHITE
-face_radius = 100
-face_position = (width // 2, height // 2)
+        # RGBからBGRに戻す
+        #image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-# Define emotions as different sets of eyes and mouths
-emotions = {
-    'happy': {'eyes': 'happy', 'mouth': 'smile'},
-    'sad': {'eyes': 'sad', 'mouth': 'frown'},
-    'angry': {'eyes': 'angry', 'mouth': 'line'}
-}
+        # 検出された手の情報を描画
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                #mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                finger_angles = FingerAngle(LandmarksToNumpy(hand_landmarks.landmark))
+                hand_shape, hand_shape_num = HandshapeJudge(finger_angles)
+                if hand_shape == 'NoSign':
+                    pass
+                else:
+                    return hand_shape, hand_shape_num
 
-# Start with a default emotion
-current_emotion = 'happy'
+        # ウィンドウに映像を表示
+        #cv2.imshow('MediaPipe Hands', image)
+        if cv2.waitKey(25) & 0xFF == 27:
+            break
 
-# Function to draw the robot face based on the current emotion
-def draw_robot_face(emotion):
-    screen.fill(BLACK)
-
-    # Draw the face
-    pygame.draw.circle(screen, BLACK, face_position, face_radius)
-
-    # Eyes
-    if emotions[emotion]['eyes'] == 'happy':
-        # Draw happy eyes
-        pygame.draw.ellipse(screen, LIGHT_BLUE, (face_position[0] - 50, face_position[1] - 20, 20, 30))
-        pygame.draw.ellipse(screen, LIGHT_BLUE, (face_position[0] + 20, face_position[1] - 20, 20, 30))
-    elif emotions[emotion]['eyes'] == 'sad':
-        # Draw sad eyes
-        pygame.draw.ellipse(screen, LIGHT_BLUE, (face_position[0] - 50, face_position[1] - 20, 30, 20))
-        pygame.draw.ellipse(screen, LIGHT_BLUE, (face_position[0] + 20, face_position[1] - 20, 30, 20))
-    elif emotions[emotion]['eyes'] == 'angry':
-        # Draw angry eyes
-        pygame.draw.line(screen, LIGHT_BLUE, (face_position[0] - 50, face_position[1] - 30), (face_position[0] - 20, face_position[1] - 20), 5)
-        pygame.draw.line(screen, LIGHT_BLUE, (face_position[0] + 50, face_position[1] - 30), (face_position[0] + 20, face_position[1] - 20), 5)
-
-    # Mouth
-    if emotions[emotion]['mouth'] == 'smile':
-        # Draw smile mouth
-        pygame.draw.arc(screen, LIGHT_BLUE, (face_position[0] - 45, face_position[1], 80, 50), 3.14, 2*3.14, 5)
-    elif emotions[emotion]['mouth'] == 'frown':
-        # Draw frown mouth
-        pygame.draw.arc(screen, LIGHT_BLUE, (face_position[0] - 40, face_position[1] + 20, 80, 50), 3.14, 2*3.14, 5)
-    elif emotions[emotion]['mouth'] == 'line':
-        # Draw line mouth
-        pygame.draw.line(screen, LIGHT_BLUE, (face_position[0] - 40, face_position[1] + 40), (face_position[0] + 40, face_position[1] + 40), 5)
-
-    pygame.display.flip()
-#######################################
-
-
-while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-        continue
-
-    # 画像をBGRからRGBに変換し、MediaPipeに渡す
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
-
-    # RGBからBGRに戻す
-    #image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-    # 検出された手の情報を描画
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            #mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-            finger_angles = FingerAngle(LandmarksToNumpy(hand_landmarks.landmark))
-            hand_shape = HandshapeJudge(finger_angles)
-            if hand_shape == 'NoSign':
-                pass
-            else:
-                draw_robot_face(hand_shape)
-            #print(hand_shape)
-            #cv2.putText(image, hand_shape_text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-            #print(type(hand_landmarks.landmark[0].x))
-
-    # ウィンドウに映像を表示
-    #cv2.imshow('MediaPipe Hands', image)
-    if cv2.waitKey(25) & 0xFF == 27:
-        break
-
-    # Pygameイベントループで終了条件をチェック
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            cap.release()
-            pygame.quit()
-            exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                cap.release()
-                pygame.quit()
-                exit()
-
-
-# リソースを解放
-cap.release()
-pygame.quit()
--- hands.py
+++ hands.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+import hand_detection as hd
 
 # Initialize Pygame
 pygame.init()
@@ -19,33 +21,126 @@ paper_image = pygame.transform.scale(paper_image, (100, 100))
 # Set the initial image to None (to display nothing)
 current_image = None
 
+###################################
+
+# Set the display to full screen
+infoObject = pygame.display.Info()
+width, height = infoObject.current_w, infoObject.current_h
+#screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
+
+
+# Define colors
+WHITE = (255, 255, 255)
+LIGHT_BLUE = (173, 216, 230)  # Color for the eyes
+BLACK = (0, 0, 0)
+GRAY = (192, 192, 192)  # Color for the face
+# Set up robot face properties
+face_color = WHITE
+face_radius = 100
+face_position = (width // 2, height // 2)
+
+
+# Define emotions as different sets of eyes and mouths
+emotions = {
+    'happy': {'eyes': 'happy', 'mouth': 'smile'},
+    'sad': {'eyes': 'sad', 'mouth': 'frown'},
+    'angry': {'eyes': 'angry', 'mouth': 'line'}
+}
+
+# Start with a default emotion
+current_emotion = 'happy'
+
+# Function to draw the robot face based on the current emotion
+def draw_robot_face(emotion):
+    screen.fill(BLACK)
+
+    # Draw the face
+    pygame.draw.circle(screen, BLACK, face_position, face_radius)
+
+    # Eyes
+    if emotions[emotion]['eyes'] == 'happy':
+        # Draw happy eyes
+        pygame.draw.ellipse(screen, LIGHT_BLUE, (face_position[0] - 50, face_position[1] - 20, 20, 30))
+        pygame.draw.ellipse(screen, LIGHT_BLUE, (face_position[0] + 20, face_position[1] - 20, 20, 30))
+    elif emotions[emotion]['eyes'] == 'sad':
+        # Draw sad eyes
+        pygame.draw.ellipse(screen, LIGHT_BLUE, (face_position[0] - 50, face_position[1] - 20, 30, 20))
+        pygame.draw.ellipse(screen, LIGHT_BLUE, (face_position[0] + 20, face_position[1] - 20, 30, 20))
+    elif emotions[emotion]['eyes'] == 'angry':
+        # Draw angry eyes
+        pygame.draw.line(screen, LIGHT_BLUE, (face_position[0] - 50, face_position[1] - 30), (face_position[0] - 20, face_position[1] - 20), 5)
+        pygame.draw.line(screen, LIGHT_BLUE, (face_position[0] + 50, face_position[1] - 30), (face_position[0] + 20, face_position[1] - 20), 5)
+
+    # Mouth
+    if emotions[emotion]['mouth'] == 'smile':
+        # Draw smile mouth
+        pygame.draw.arc(screen, LIGHT_BLUE, (face_position[0] - 45, face_position[1], 80, 50), 3.14, 2*3.14, 5)
+    elif emotions[emotion]['mouth'] == 'frown':
+        # Draw frown mouth
+        pygame.draw.arc(screen, LIGHT_BLUE, (face_position[0] - 40, face_position[1] + 20, 80, 50), 3.14, 2*3.14, 5)
+    elif emotions[emotion]['mouth'] == 'line':
+        # Draw line mouth
+        pygame.draw.line(screen, LIGHT_BLUE, (face_position[0] - 40, face_position[1] + 40), (face_position[0] + 40, face_position[1] + 40), 5)
+
+    pygame.display.flip()
+
+
+
 # Main loop
 running = True
+current_emotion = 'happy'
+current_image = paper_image
+
+# Clear the screen
+screen.fill((0, 0, 0))
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             running = False
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    exit()
 
-        # Change image on key press
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_1:
-                current_image = rock_image
-            elif event.key == pygame.K_2:
-                current_image = scissors_image
-            elif event.key == pygame.K_3:
-                current_image = paper_image
-            elif event.key == pygame.K_0:
-                current_image = None  # Set to None to display nothing
+    key = random.randrange(3)
+    if key == 0:
+        current_image = rock_image
+    elif key == 1:
+        current_image = scissors_image
+    elif key == 2:
+        current_image = paper_image
 
-    # Clear the screen
-    screen.fill((0, 0, 0))
+    # Get hand shape
+    current_emotion, current_emotion_num = hd.GetHandShape()
+    judge = ( key - current_emotion_num + 3 ) % 3
+    print(current_emotion, current_emotion_num, key, judge)
+
+    if judge == 0:
+        print('Draw')
+    elif judge == 1:
+        print('Win')
+    elif judge == 2:
+        print('Lose')
+    #print(current_emotion)
+
+    draw_robot_face(current_emotion)
 
     # Draw the current image if it's not None
-    if current_image is not None:
-        screen.blit(current_image, (50, 50))  # Draw the image at position (50, 50)
+    #if current_image is not None:
+    screen.blit(current_image, (50, 150))  # Draw the image at position (50, 50)
+    pygame.display.update()
+    pygame.time.wait(1000)
+
 
     # Update the display
-    pygame.display.flip()
+    #pygame.display.flip()
+    pygame.display.update()
+    print(1)
+
 
 # Quit pygame
 pygame.quit()
